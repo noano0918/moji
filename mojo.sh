148684e611eeafd1c3b46a7f638aa8bc43b944ee
noano0918 <100474140+noano0918@users.noreply.github.com>
2022-02-26 18:24:11 +0330

    Update mojo.sh

--- mojo.sh
+++ mojo.sh
@@ -7,8 +7,8 @@ echo -e " ........................ \e[1;93m"
 echo -e "   ....................   \e[1;97m"
 echo -e "............       \e[1;97m"
 echo ""
-echo -e "\e[1;91m [+] telegram : \e[1;92mMR_x_moji"
-echo -e "\e[1;91m [+] Github: \e[1;92mmoji-MRX\e[1;97m"
+echo -e "\e[1;91m [+] naem : \e[1;92mmojtaba moradi"
+echo -e "\e[1;91m [+] id rubika : \e[1;92m@vaseteh_moji\e[1;97m"
 echo ""
 read -p " Does you want to install Parrot Shell in Termux?(Yes/No) : " input
 
