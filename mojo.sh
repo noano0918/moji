4eafaac3b36ba15b56064a42a3e359ee70c4b3f8
noano0918 <100474140+noano0918@users.noreply.github.com>
2022-02-26 18:55:28 +0330

    Update mojo.sh

--- mojo.sh
+++ mojo.sh
@@ -1,7 +1,7 @@
 
 clear
 echo ""
-echo -e "\e[1;91m Long live my country 🇮🇷 "
+echo -e "\e[1;91m Long live my country \e[1;92miran "
 echo ""
 echo -e "\e[1;91m [+] naem : \e[1;92mmojtaba moradi"
 echo -e "\e[1;91m [+] id rubika : \e[1;92m@vaseteh_moji\e[1;97m"
